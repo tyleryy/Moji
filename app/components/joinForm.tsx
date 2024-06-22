df461f74e3e4c21cbce1918ecd45c5b5c5de62b5
Tyler Yu <tyleryy@uci.edu>
2024-06-22 15:10:21 -0700

    fix hyrdation err

--- app/components/joinForm.tsx
+++ app/components/joinForm.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import Image from "next/image";
-import { Input, Stack, Button } from '@chakra-ui/react'
+import { Input, Stack, Button } from "@chakra-ui/react";
 import { join } from "path";
 import { useState } from "react";
 
 export default function JoinForm() {
   const [roomName, setRoomName] = useState("");
 
-  function joinRoom() {
-
-  }
+  function joinRoom() {}
 
   return (
-        <Stack spacing={3}>
-        <Input placeholder='Enter room name' bg="transparent" color="white" value={roomName} />
-        <Input placeholder='Enter username' bg="transparent" color="white" />
-        <Button onClick={joinRoom}>Submit</Button>
-        </Stack>
+    <Stack spacing={3}>
+      <Input
+        placeholder="Enter room name"
+        bg="transparent"
+        color="white"
+        value={roomName}
+      />
+      <Input placeholder="Enter username" bg="transparent" color="white" />
+      <Button onClick={joinRoom}>Submit</Button>
+    </Stack>
   );
 }
