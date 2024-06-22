4dc3b037f6462db2ff118aa95df8bfe9556343d8
maithyy <maithyle265@yahoo.com>
2024-06-22 14:39:16 -0700

    Add basics of username and room name submission

---
+++ mast/app/components/joinForm.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import { Input, Stack, Button } from '@chakra-ui/react'
+import { join } from "path";
+import { useState } from "react";
+
+export default function JoinForm() {
+  const [roomName, setRoomName] = useState("");
+
+  function joinRoom() {
+
+  }
+
+  return (
+        <Stack spacing={3}>
+        <Input placeholder='Enter room name' bg="transparent" color="white" value={roomName} />
+        <Input placeholder='Enter username' bg="transparent" color="white" />
+        <Button onClick={joinRoom}>Submit</Button>
+        </Stack>
+  );
+}
