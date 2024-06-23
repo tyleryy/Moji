247661b2141cf1455b7c884c91629b53c241a235
Tyler Yu <tyleryy@uci.edu>
2024-06-23 00:51:58 -0700

    switched to api

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: "/server/:path*",
-          destination:
-            process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/server/:path*"
-              : "/server/",
-        },
-      ];
-    },
-  };
-  
-export default nextConfig
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
