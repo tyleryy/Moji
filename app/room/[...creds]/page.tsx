cb84077dd7489cba6872114c7f51f778173ca8e8
maithyy <maithyle265@yahoo.com>
2024-06-23 00:10:06 -0700

    Implement automatic screenshot

--- app/room/[...creds]/page.tsx
+++ app/room/[...creds]/page.tsx
@@ -51,15 +51,18 @@ export default function Page({ params }: { params: { creds: string[] } }) {
   };
 
   const captureAndUploadImage = async () => {
-  const captureAndUploadImage = async () => {
     if (ref.current) {
       const screenshot = await takeScreenShot(ref.current);
       const file = await imageToFile(screenshot);
       console.log("Captured file:", file);
       setFile(file);
       await uploadFileToSupabase(file);
+      const screenshot = await takeScreenShot(ref.current);
+      const file = await imageToFile(screenshot);
+      console.log("Captured file:", file);
+      setFile(file);
+      await uploadFileToSupabase(file);
     } else {
-      console.error("The ref is not correctly set.");
       console.error("The ref is not correctly set.");
     }
   };
@@ -83,6 +86,25 @@ export default function Page({ params }: { params: { creds: string[] } }) {
   }, []);
 
   useEffect(() => {
+    const intervalId = setInterval(() => {
+      captureAndUploadImage();
+    }, 10000); // 10 seconds interval
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (ref.current) {
+        console.log("Taking screenshot...")
+        takeScreenShot(ref.current);
+      } else {
+        console.error("The ref is not correctly set.");
+      }
+    }, 20000); // 10 seconds interval
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, [ref, takeScreenShot]);
     const intervalId = setInterval(() => {
       captureAndUploadImage();
     }, 10000); // 10 seconds interval
@@ -126,6 +148,7 @@ export default function Page({ params }: { params: { creds: string[] } }) {
         share tracks and to leave the room. */}
         <ControlBar />
       </LiveKitRoom>
+      {/* <Button onClick={getImage}>Take picture</Button> */}
       {/* <Button onClick={getImage}>Take picture</Button> */}
     </div>
   );
